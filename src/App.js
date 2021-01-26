1545ace06fcbe7733498bbef664e80f195d205c2
angfrase <angela.n.fraser@gmail.com>
2021-01-26 13:58:55 -0500

    images and audio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Game from './components/Game/game'
+import bunny from './Assets/images/bunny.png'
+import hound from './Assets/images/hound.png'
+import raccoon from './Assets/images/raccoon.png'
+import squirrel from './Assets/images/squirrel.png'
+
+
+
+function App(props) {
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: raccoon,
+  }
+
+  const squirrels = {
+    name: 'Sheridan Squirrels',
+    logoSrc: squirrel,
+  }
+
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: bunny,
+  }
+
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: hound,
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
     </div>
-  );
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
